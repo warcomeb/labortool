21f51c25aee639d86b2ec771ab1668bb986e36bc
Marco Giammarini <m.giammarini@warcomeb.it>
2014-11-07 13:51:13 +0100

    Ultimata l'implementazione della classe ActivityNote.

--- activitynote.cpp
+++ activitynote.cpp
@@ -1,6 +1,58 @@
 #include "activitynote.h"
 
-ActivityNote::ActivityNote(QString text)
+ActivityNote::ActivityNote(QString text, Activity* activity, Employee* author)
+{
+    m_id = 0;
+
+    m_activityId = activity->getId();
+
+    m_text = text;
+
+    m_creationEmployee = m_modificationEmployee = author->getId();
+    m_creationDate = m_modificationDate = QDateTime::currentDateTime();
+}
+
+ActivityNote::ActivityNote()
+{
+    m_id = 0;
+    m_text = "";
+}
+
+void ActivityNote::setId (uint id)
+{
+    m_id = id;
+}
+
+void ActivityNote::setActivityId (uint id)
+{
+    m_activityId = id;
+}
+
+void ActivityNote::setCreationInformation (Employee* author, QDateTime date)
+{
+    m_creationEmployee = author->getId();
+    m_creationDate = date;
+}
+
+void ActivityNote::setCreationInformation (Employee* author, QString date)
+{
+    m_creationEmployee = author->getId();
+    m_creationDate = QDateTime::fromString(date,"yyyy-MM-dd hh:mm:ss");
+}
+
+void ActivityNote::setModificationInformation (Employee* author, QDateTime date)
+{
+    m_modificationEmployee = author->getId();
+    m_modificationDate = date;
+}
+
+void ActivityNote::setModificationInformation (Employee* author, QString date)
+{
+    m_modificationEmployee = author->getId();
+    m_modificationDate = QDateTime::fromString(date,"yyyy-MM-dd hh:mm:ss");
+}
+
+void ActivityNote::setText(QString text)
 {
     m_text = text;
 }
--- activitynote.h
+++ activitynote.h
@@ -2,18 +2,51 @@
 #define ACTIVITYNOTE_H
 
 #include <QString>
-#include <QDate>
+#include <QDateTime>
+
+#include "activity.h"
+#include "employee.h"
 
 class ActivityNote
 {
 public:
-    ActivityNote(QString text);
+    ActivityNote(QString text, Activity* activity, Employee* author);
+    ActivityNote();
+
+    uint getId () const { return m_id; }
+    uint getActivityId () const { return m_activityId; }
+
+    QString getText() const {return m_text; }
+
+    QDateTime getCreationDate () const { return m_creationDate; }
+    uint getCreationEmployee () const { return m_creationEmployee; }
+
+    QDateTime getModificationDate () const { return m_modificationDate; }
+    uint getModificationEmployee () const { return m_modificationEmployee; }
+
+    void setId (uint id);
+    void setActivityId (uint id);
+
+    void setCreationInformation (Employee* author, QDateTime date);
+    void setCreationInformation (Employee* author, QString date);
+
+    void setModificationInformation (Employee* author, QDateTime date);
+    void setModificationInformation (Employee* author, QString date);
+
+    void setText(QString text);
 
 private:
     uint m_id;
 
+    uint m_activityId;
+
     QString m_text;
-    QDate   m_registration;
+
+    uint m_creationEmployee;
+    uint m_modificationEmployee;
+
+    QDateTime m_creationDate;
+    QDateTime m_modificationDate;
 };
 
 #endif // ACTIVITYNOTE_H
