20d1a77ba39582a4073121238a48c2d3ed9839a9
BetaDroid <danielzarioiu@gmail.com>
2016-08-09 17:31:38 +0200

    Add files via upload

--- 04-Sources/labortool-client_v0.1-beta/app/main.js
+++ 04-Sources/labortool-client_v0.1-beta/app/main.js
@@ -3,10 +3,13 @@
 var platform_browser_dynamic_1 = require('@angular/platform-browser-dynamic');
 var http_1 = require('@angular/http');
 var app_component_1 = require('./app.component');
+var forms_1 = require('@angular/forms');
 var app_routes_1 = require('./app.routes');
 platform_browser_dynamic_1.bootstrap(app_component_1.AppComponent, [
     app_routes_1.appRouterProviders,
-    http_1.HTTP_PROVIDERS
+    http_1.HTTP_PROVIDERS,
+    forms_1.disableDeprecatedForms(),
+    forms_1.provideForms()
 ])
     .catch(function (err) { return console.error(err); });
 //# sourceMappingURL=main.js.map
--- 04-Sources/labortool-client_v0.1-beta/app/main.ts
+++ 04-Sources/labortool-client_v0.1-beta/app/main.ts
@@ -1,14 +1,17 @@
 // main entry point
-import { bootstrap }          from '@angular/platform-browser-dynamic';
+import { bootstrap }                              from '@angular/platform-browser-dynamic';
 
-import {HTTP_PROVIDERS, Http, URLSearchParams} from '@angular/http';
+import {HTTP_PROVIDERS, Http, URLSearchParams}    from '@angular/http';
 
-import { AppComponent }       from './app.component';
+import { AppComponent }                           from './app.component';
+import { disableDeprecatedForms, provideForms }   from '@angular/forms';
 
-import { appRouterProviders } from './app.routes';
+import { appRouterProviders }                     from './app.routes';
 
 bootstrap(AppComponent, [
   appRouterProviders,
-  HTTP_PROVIDERS
+  HTTP_PROVIDERS,
+  disableDeprecatedForms(),
+  provideForms()
 ])
 .catch(err => console.error(err));
