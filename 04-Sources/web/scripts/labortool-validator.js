7ef712d697d96b8609694dadb460bb7d60d5d7d8
Marco Giammarini <marco.giammarini@gruppofilippetti.it>
2019-01-23 14:11:43 +0100

    Working on adding customer/supplier page

--- 04-Sources/web/scripts/labortool-validator.js
+++ 04-Sources/web/scripts/labortool-validator.js
@@ -24,84 +24,121 @@
  *  Marco Giammarini <m.giammarini@warcomeb.it>
  */
 
-//$(document).ready(function() 
-//{
-	// New rule validator
-	window.Parsley
-	  .addValidator('provincia', {
-	      requirementType: 'string',
-	      validateString: function(value, requirement) 
-	      {
-	    	      reg = /^[a-zA-Z]{2}$/;
-	          return reg.test(value);
-	      },
-	      messages: {
-	          en: 'This string should be only two char',
-	          it: 'La sigla della Provincia deve essere di sole due lettere'
-	      }
-	  })
-	  .addValidator('zipCode', {
-	      requirementType: 'string',
-	      validateString: function(value, requirement) 
-	      {
-	    	      reg = /^[0-9]{5}$/;
-	          return reg.test(value);
-	      },
-	      messages: {
-	          en: 'The ZIP code should be five number',
-	          it: 'Il CAP deve essere costituito da 5 numeri'
-	      }
-	  })
-	  .addValidator('nin', {
-	      requirementType: 'string',
-	      validateString: function(value, requirement) 
-	      {
-	    	      reg = /^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$/;
-	          return reg.test(value);
-	      },
-	      messages: {
-	          en: 'The NIN code should be in Italian format',
-	          it: 'Il formato del codice fiscale è sbagliato'
-	      }
-	  })
-	  .addValidator('phone', {
-	      requirementType: 'string',
-	      validateString: function(value, requirement) 
-	      {
-	    	      reg = /^(\+[0-9]{1,3})?( |\-)?[0-9]{1,3}( |\-)?[0-9]{3,5}( |\-)?[0-9]{3,4}(( |\-)?[0-9]{3})?$/;
-	          return reg.test(value);
-	      },
-	      messages: {
-	          en: 'The phone number was in the wrong format',
-	          it: 'Il numero di telefono non è valido'
-	      }
-	  })
-	  .addValidator('password', {
-	      requirementType: 'string',
-	      validateString: function(value, requirement) 
-	      {
-	    	      reg = /^[0-9a-zA-Z]{8,}$/;
-	          return reg.test(value);
-	      },
-	      messages: {
-	          en: 'The password must containat least 8 alphanumeric characters',
-	          it: 'La password deve contenere almeno 8 caratteri alfanumerici'
-	      }
-	  })
-	  .addValidator('birthday', {
-          validateString: function(value, minAge) 
-          {
-              var birthday = moment(value, "DD/MM/YYYY");
-              var today = moment(new Date());
+// New rule validator
+window.Parsley
+    .addValidator('provincia', {
+        requirementType: 'string',
+        validateString: function(value, requirement) 
+        {
+            reg = /^[a-zA-Z]{2}$/;
+            return reg.test(value);
+        },
+        messages: {
+            en: 'This string should be only two char',
+            it: 'La sigla della Provincia deve essere di sole due lettere'
+        }
+    })
+    .addValidator('zipCode', {
+        requirementType: 'string',
+        validateString: function(value, requirement) 
+        {
+            reg = /^[0-9]{5}$/;
+            return reg.test(value);
+        },
+        messages: {
+            en: 'The ZIP code should be five number',
+            it: 'Il CAP deve essere costituito da 5 numeri'
+        }
+    })
+    .addValidator('nin', {
+        requirementType: 'string',
+        validateString: function(value, requirement) 
+        {
+            reg = /^[a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z]$/;
+            return reg.test(value);
+        },
+        messages: {
+            en: 'The NIN code should be in Italian format',
+            it: 'Il formato del codice fiscale è sbagliato'
+        }
+    })
+    .addValidator('ninsupplier', {
+        requirementType: 'string',
+        validateString: function(value, requirement)
+        {
+            reg = /^(ATU[0-9]{8}|BE0[0-9]{9}|BG[0-9]{9,10}|CY[0-9]{8}L|CZ[0-9]{8,10}|DE[0-9]{9}|DK[0-9]{8}|EE[0-9]{9}|(EL|GR)[0-9]{9}|ES[0-9A-Z][0-9]{7}[0-9A-Z]|FI[0-9]{8}|FR[0-9A-Z]{2}[0-9]{9}|GB([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|HU[0-9]{8}|IE[0-9]S[0-9]{5}L|IT[0-9]{11}|LT([0-9]{9}|[0-9]{12})|LU[0-9]{8}|LV[0-9]{11}|MT[0-9]{8}|NL[0-9]{9}B[0-9]{2}|PL[0-9]{10}|PT[0-9]{9}|RO[0-9]{2,10}|SE[0-9]{12}|SI[0-9]{8}|SK[0-9]{10})|([a-zA-Z]{6}[0-9]{2}[a-zA-Z][0-9]{2}[a-zA-Z][0-9]{3}[a-zA-Z])$/;
+            return reg.test(value); 
+        },
+        messages: 
+        {
+            en: 'The NIN code shuld be a VATID or NIN in Italian format',
+            it: 'Il formato del codice fiscale è sbagliato'
+        }
+    })
+    .addValidator('vat', {
+        requirementType: 'string',
+        validateString: function(value, requirement)
+        {
+            reg = /^(ATU[0-9]{8}|BE0[0-9]{9}|BG[0-9]{9,10}|CY[0-9]{8}L|CZ[0-9]{8,10}|DE[0-9]{9}|DK[0-9]{8}|EE[0-9]{9}|(EL|GR)[0-9]{9}|ES[0-9A-Z][0-9]{7}[0-9A-Z]|FI[0-9]{8}|FR[0-9A-Z]{2}[0-9]{9}|GB([0-9]{9}([0-9]{3})?|[A-Z]{2}[0-9]{3})|HU[0-9]{8}|IE[0-9]S[0-9]{5}L|IT[0-9]{11}|LT([0-9]{9}|[0-9]{12})|LU[0-9]{8}|LV[0-9]{11}|MT[0-9]{8}|NL[0-9]{9}B[0-9]{2}|PL[0-9]{10}|PT[0-9]{9}|RO[0-9]{2,10}|SE[0-9]{12}|SI[0-9]{8}|SK[0-9]{10})$/;
+            return reg.test(value); 
+        },
+        messages: 
+        {
+            en: 'The VATID code is not of a European Country',
+            it: 'Il formato della P.IVA è sbagliato'
+        }
+    })
+    .addValidator('iban', {
+        requirementType: 'string',
+        validateString: function(value, requirement)
+        {
+            reg = /^(?:(?:IT|SM)\d{2}[A-Z]\d{22}|CY\d{2}[A-Z]\d{23}|NL\d{2}[A-Z]{4}\d{10}|LV\d{2}[A-Z]{4}\d{13}|(?:BG|BH|GB|IE)\d{2}[A-Z]{4}\d{14}|GI\d{2}[A-Z]{4}\d{15}|RO\d{2}[A-Z]{4}\d{16}|KW\d{2}[A-Z]{4}\d{22}|MT\d{2}[A-Z]{4}\d{23}|NO\d{13}|(?:DK|FI|GL|FO)\d{16}|MK\d{17}|(?:AT|EE|KZ|LU|XK)\d{18}|(?:BA|HR|LI|CH|CR)\d{19}|(?:GE|DE|LT|ME|RS)\d{20}|IL\d{21}|(?:AD|CZ|ES|MD|SA)\d{22}|PT\d{23}|(?:BE|IS)\d{24}|(?:FR|MR|MC)\d{25}|(?:AL|DO|LB|PL)\d{26}|(?:AZ|HU)\d{27}|(?:GR|MU)\d{28})$/;
+            return reg.test(value); 
+        },
+        messages: 
+        {
+            en: 'The IBAN code is not correct',
+            it: 'Il formato dell\'IBAN è sbagliato'
+        }
+    })
+    .addValidator('phone', {
+        requirementType: 'string',
+        validateString: function(value, requirement) 
+        {
+            reg = /^(\+[0-9]{1,3})?( |\-)?[0-9]{1,3}( |\-)?[0-9]{3,5}( |\-)?[0-9]{3,4}(( |\-)?[0-9]{3})?$/;
+            return reg.test(value);
+        },
+        messages: {
+            en: 'The phone number was in the wrong format',
+            it: 'Il numero di telefono non è valido'
+        }
+    })
+    .addValidator('password', {
+        requirementType: 'string',
+        validateString: function(value, requirement) 
+        {
+            reg = /^[0-9a-zA-Z]{8,}$/;
+            return reg.test(value);
+        },
+        messages: {
+            en: 'The password must containat least 8 alphanumeric characters',
+            it: 'La password deve contenere almeno 8 caratteri alfanumerici'
+        }
+    })
+    .addValidator('birthday', {
+        validateString: function(value, minAge) 
+        {
+            var birthday = moment(value, "DD/MM/YYYY");
+            var today = moment(new Date());
 
-              // Check only if the birthday date is greater then today
-              var test = today.diff(birthday);
-              if (test > 0) return true;
-              else return false;
-	      },
-	      messages: {
-	          en: 'Birthday must not be greater than today',
-	          it: 'La data di nascita deve essere antecedente ad oggi'
-	      }
-	  });
+            // Check only if the birthday date is greater then today
+            var test = today.diff(birthday);
+            if (test > 0) return true;
+            else return false;
+        },
+        messages: {
+            en: 'Birthday must not be greater than today',
+            it: 'La data di nascita deve essere antecedente ad oggi'
+        }
+    });
 //});
--- 04-Sources/web/scripts/labortool.js
+++ 04-Sources/web/scripts/labortool.js
@@ -29,7 +29,10 @@ $(document).ready(function()
     $('#labortool-logout').on("click",userLogout);
 
     // Data table setup
-    $('#user-list-table').DataTable();
+    if ($('#user-list-table').length)
+    {
+        $('#user-list-table').DataTable();
+    }
 
     // Datetime picker enable
     moment.locale('it');
@@ -37,66 +40,86 @@ $(document).ready(function()
     	    format: 'DD/MM/YYYY',
     });
 
-    $('#add-user').parsley().on('form:success', function() 
+    if ($('#add-user').length)
     {
-    	    // TODO: do somethings?
-    })
-    .on('form:error', function() 
-    	{
-	    // TODO: do somethings?
-    })
-    .on('form:submit', function() 
-    {
-	    var formData = {
-	        'name'              : $('#inputName').val(),
-	        'surname'           : $('#inputSurname').val(),
-	        'password'          : CryptoJS.SHA256($('#inputPassword').val()).toString(CryptoJS.enc.Hex),
-	        'email'             : $('#inputEmail').val(),
-	        'phone'             : $('#inputPhone').val(),
-	        'address'           : $('#inputAddress').val(),
-	        'city'              : $('#inputCity').val(),
-	        'district'          : $('#inputDistrict').val(),
-	        'zip'               : $('#inputZip').val(),
-	        'country'           : $('#inputCountry').val(),
-	        'nin'               : $('#inputNIN').val(),
-	        'birthday'          : $('#inputBirthday').val(),
-	        'note'              : $('#inputNote').val(),
-	    };
-    
-	    // Disable buttons
-        $('#add-user-save').attr("disabled","disabled");
-        $('#add-user-reset').attr("disabled","disabled");
-        // Show loading gift
-	    $('#add-user-loading').removeClass('d-none').delay(800);
-	    
-        $.ajax({
-            type        : 'POST',
-            url         : '/prjform/adduser.form.php',
-            data        : formData, 
-            dataType    : 'json',
-            encode      : true
-        })
-        .done(function(data)
+        console.log("INFO: #add-user exist!");
+
+        $('#add-user').parsley().on('form:success', function() 
         {
-            // Just for debug
-            //console.log(data);
-
-            // NO ERRORS
-            if (0 === data['status'])
+                // TODO: do somethings?
+        })
+        .on('form:error', function() 
             {
-                // Show success message
-                $('#add-user-loading').addClass('d-none');
-                $('#add-user-error').addClass('d-none');
-                $('#add-user-success').removeClass('d-none').delay(2000);
+            // TODO: do somethings?
+        })
+        .on('form:submit', function() 
+        {
+            var formData = {
+                'name'              : $('#inputName').val(),
+                'surname'           : $('#inputSurname').val(),
+                'password'          : CryptoJS.SHA256($('#inputPassword').val()).toString(CryptoJS.enc.Hex),
+                'email'             : $('#inputEmail').val(),
+                'phone'             : $('#inputPhone').val(),
+                'address'           : $('#inputAddress').val(),
+                'city'              : $('#inputCity').val(),
+                'district'          : $('#inputDistrict').val(),
+                'zip'               : $('#inputZip').val(),
+                'country'           : $('#inputCountry').val(),
+                'nin'               : $('#inputNIN').val(),
+                'birthday'          : $('#inputBirthday').val(),
+                'note'              : $('#inputNote').val(),
+            };
+        
+            // Disable buttons
+            $('#add-user-save').attr("disabled","disabled");
+            $('#add-user-reset').attr("disabled","disabled");
+            // Show loading gift
+            $('#add-user-loading').removeClass('d-none').delay(800);
+            
+            $.ajax({
+                type        : 'POST',
+                url         : '/prjform/adduser.form.php',
+                data        : formData, 
+                dataType    : 'json',
+                encode      : true
+            })
+            .done(function(data)
+            {
+                // Just for debug
+                //console.log(data);
 
-                // Redirect to view page...
-                $(location).attr('href', '/records/employee/view/' + data['id']);
-            }
-            else if (1 === data['status'])
+                // NO ERRORS
+                if (0 === data['status'])
+                {
+                    // Show success message
+                    $('#add-user-loading').addClass('d-none');
+                    $('#add-user-error').addClass('d-none');
+                    $('#add-user-success').removeClass('d-none').delay(2000);
+
+                    // Redirect to view page...
+                    $(location).attr('href', '/records/employee/view/' + data['id']);
+                }
+                else if (1 === data['status'])
+                {
+                    // Write text into error div message
+                    $('#add-user-error').html('<i class="fas fa-times"></i>');
+                    $('#add-user-error').append(' ' + data['status']);
+
+                    // Show error message
+                    $('#add-user-loading').addClass('d-none');
+                    $('#add-user-success').addClass('d-none');
+                    $('#add-user-error').removeClass('d-none').delay(2000);
+
+                    // Enable buttons
+                    $('#add-user-save').removeAttr("disabled");
+                    $('#add-user-reset').removeAttr("disabled");
+                }
+            })
+            .fail(function(jqXHR, textStatus)
             {
                 // Write text into error div message
                 $('#add-user-error').html('<i class="fas fa-times"></i>');
-                $('#add-user-error').append(' ' + data['status']);
+                $('#add-user-error').append(' ' + textStatus);
 
                 // Show error message
                 $('#add-user-loading').addClass('d-none');
@@ -106,27 +129,52 @@ $(document).ready(function()
                 // Enable buttons
                 $('#add-user-save').removeAttr("disabled");
                 $('#add-user-reset').removeAttr("disabled");
-            }
-        })
-        .fail(function(jqXHR, textStatus)
+            });
+
+            // Don't submit form
+            return false;
+        });
+    }
+
+    if ($('#add-customer').length)
+    {
+        console.log("INFO: #add-customer exist!");
+
+        $('#add-customer').parsley().on('form:success', function() 
         {
-            // Write text into error div message
-            $('#add-user-error').html('<i class="fas fa-times"></i>');
-            $('#add-user-error').append(' ' + textStatus);
-
-            // Show error message
-            $('#add-user-loading').addClass('d-none');
-            $('#add-user-success').addClass('d-none');
-            $('#add-user-error').removeClass('d-none').delay(2000);
-
-            // Enable buttons
-            $('#add-user-save').removeAttr("disabled");
-            $('#add-user-reset').removeAttr("disabled");
-        	});
-
-        // Don't submit form
-        return false;
-    });
+                // TODO: do somethings?
+        })
+        .on('form:error', function() 
+            {
+            // TODO: do somethings?
+        })
+        .on('form:submit', function() 
+        {
+            var formData = {
+                'name'              : $('#inputName').val(),
+                'address'           : $('#inputAddress').val(),
+                'city'              : $('#inputCity').val(),
+                'district'          : $('#inputDistrict').val(),
+                'zip'               : $('#inputZip').val(),
+                'country'           : $('#inputCountry').val(),
+                'website'           : $('#inputWebsite').val(),
+                'email'             : $('#inputEmail').val(),
+                'phone'             : $('#inputPhone').val(),
+                'fax'               : $('#inputFax').val(),
+                'vat'               : $('#inputVAT').val(),
+                'nin'               : $('#inputNIN').val(),
+                'iban'              : $('#inputIBAN').val(),
+                'note'              : $('#inputNote').val(),
+                'customer'          : $("#inputCustomerCheck").is(":checked") ? 1 : 0,
+                'supplier'          : $("#inputSupplierCheck").is(":checked") ? 1 : 0,
+                'private'           : $("#inputPrivateCheck").is(":checked") ? 1 : 0,
+            };
+           
+           
+            // Don't submit form
+            return false;
+        });
+    }
 
     /*
      * User logout function
